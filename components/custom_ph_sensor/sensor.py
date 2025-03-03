faec2e79a2ef01bbf2dacac8bcc6f1d8514a8b4e
00silun <80539080+00silun@users.noreply.github.com>
2025-03-03 22:25:33 +0100

    Update sensor.py

--- components/custom_ph_sensor/sensor.py
+++ components/custom_ph_sensor/sensor.py
@@ -3,26 +3,6 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID
 
-# Create a namespace matching your C++ namespace structure.
-esp_home_ns = cg.esphome_ns.namespace('esp_home')
-custom_ph_sensor_ns = esp_home_ns.namespace('custom_ph_sensor')
-
-# Declare your C++ class.
-PhSensor = custom_ph_sensor_ns.class_('PhSensor', cg.PollingComponent, sensor.Sensor)
-
-# Define the configuration schema; now only requiring the ADS sensor dependency.
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(PhSensor),
-    cv.Required("ads_sensor"): cv.use_id(sensor.Sensor),
-}).extend(sensor.sensor_schema(PhSensor))
-
-
-
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import sensor
-from esphome.const import CONF_ID
-
 custom_ph_sensor_ns = cg.esphome_ns.namespace("custom_ph_sensor")
 
 # Declare your C++ class 
@@ -42,6 +22,10 @@ async def to_code(config):
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
     
+    # Register service for pH 7 calibration
+    cg.add(var.register_service("calibrate_ph7", {}, lambda: var.calibrate_neutral()))
+    # Register service for pH 4 calibration
+    cg.add(var.register_service("calibrate_ph4", {}, lambda: var.calibrate_acid()))
     
     
     
